a6d3c402df6d7726584df009b316567560e7ff8c
farahayar <ayari.farah98@gmail.com>
2019-07-31 09:51:05 +0100

    +shoppingCart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/ecom/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    redirectTo:"Home",
+    pathMatch:"full"
+  },
+  {
+    path:"Home",
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,49 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryListComponent } from './components/admin/category-list/category-list.component';
+import { CategoryAddComponent } from './components/admin/category-add/category-add.component';
+import { CategoryUpdateComponent } from './components/admin/category-update/category-update.component';
+import { HomeComponent } from './components/ecom/home/home.component';
+import { ConnectComponent } from './components/ecom/connect/connect.component';
+import { SubscribeComponent } from './components/ecom/subscribe/subscribe.component';
+import { ProductAddComponent } from './components/seller/product-add/product-add.component';
+import { ProductListComponent } from './components/seller/product-list/product-list.component';
+import { ProductUpdateComponent } from './components/seller/product-update/product-update.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EcomService } from './services/ecom.service';
+import { AdminGuard } from './guards/admin.guard';
+import { AdminService } from './services/admin.service';
+import { SellerService } from './services/seller.service';
+import { UserService } from './services/user.service';
+import { ShoppingCartComponent } from './src/app/components/ecom/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryListComponent,
+    CategoryAddComponent,
+    CategoryUpdateComponent,
+    HomeComponent,
+    ConnectComponent,
+    SubscribeComponent,
+    ProductAddComponent,
+    ProductListComponent,
+    ProductUpdateComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    EcomService,
+    AdminService,
+    SellerService,
+    UserService
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
